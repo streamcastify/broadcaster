48d589abf8acbf2c03c9c91787d8141d8b3a5b35
streamcastify <webmaster@streamcastify.com>
2016-08-25 14:36:02 -0700

    Update START_BROADCAST.sh

--- START_BROADCAST.sh
+++ START_BROADCAST.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# Log activity of serial port, pushing changes to a logfile.
 (stty 1200 cs8 -ixon; cat) < /dev/ttyS0 > /home/studio/logs/ttyS0.log &
+
+# Monitor changes to ttyS0.log and run the commercial script on change.
+nohup fileschanged -f /home/studio/logs/ttyS0.log  -exec=/home/studio/commercial &
+
+# Run liquidsoap with ON_AIR config.
 /usr/bin/liquidsoap /home/studio/ON_AIR.liq
 
 exit 0
