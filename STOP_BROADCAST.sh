8b8bfdd91bf0e070157eee1966075dab7f696ba9
streamcastify <webmaster@streamcastify.com>
2016-08-25 14:37:51 -0700

    Update STOP_BROADCAST.sh

--- STOP_BROADCAST.sh
+++ STOP_BROADCAST.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-
 # Kill the running instance of ON_AIR daemon
 /bin/kill  ps axf | grep "/usr/bin/liquidsoap /home/studio/ON_AIR" | grep -v grep | awk '{print "kill -9 " $1}'
+
+# Need command sto accurately also stop logging and file change monitoring.
 
 exit 0
